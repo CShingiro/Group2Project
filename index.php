44412857282e3badebd3ef135cdd0edae24591ac
CShingiro <cshingiro7202@conestogac.on.ca>
2021-09-27 19:41:27 -0400

    Skeleton of Site

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <link href="site.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+
+    </body>
+</html>

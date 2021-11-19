45a1d0fe95a49473c28164c2bf2ade4600e5a9df
CShingiro <cshingiro7202@conestogac.on.ca>
2021-11-19 14:08:13 -0500

    Change to MessagesID in SQL Database

--- Php+SQLFiles/users.sql
+++ Php+SQLFiles/users.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `Contact`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Contact` (
-  `MessageID` int NOT NULL,
+  `MessageID` int NOT NULL AUTO_INCREMENT,
   `Name` varchar(45) DEFAULT NULL,
   `Email` varchar(45) DEFAULT NULL,
   `Subject` varchar(45) DEFAULT NULL,

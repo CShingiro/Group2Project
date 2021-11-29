71d55a3543fc1dcfa2948e03eaa1606bf3a08dce
CShingiro <cshingiro7202@conestogac.on.ca>
2021-11-29 17:38:58 -0500

    JQuery changes

--- shop.js
+++ shop.js
@@ -1,17 +1,23 @@
 "use strict";
 
-(() => {
-    $("a").click(function(){
-        localStorage.setItem("image", this.prev().attr('src'));
-    });
-});
+$(document).ready(function() {
+    $("a").click(function(e) {
+      e.preventDefault();
+      let src = $(this).closest('figure').find('img').attr('src');
+      localStorage.setItem("useImage", src);
+      console.log('src', src);
+      // now that we got it stored, allow the link to go through
+      window.location.href=$(this).attr('href');
+    })
+  });
 
 //on next page
 
-(() => {
-    if (!localStorage.getItem("image")) return;
+$(document).ready(function() {
+    if (!localStorage.getItem("useImage")) return;
     let img = $('<img />', {
-        src: localStorage.getItem("image"),
+      src: localStorage.getItem("useImage"),
     });
     img.appendTo($('#product'));
-});
+  });
+

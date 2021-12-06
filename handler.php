50b50382e33d34b20afb4a0a7e52179acf20431b
CShingiro <cshingiro7202@conestogac.on.ca>
2021-12-06 03:42:20 -0500

    Changes to site

--- handler.php
+++ handler.php
@@ -1,10 +1,15 @@
 <?php
     ini_set('allow_url_fopen',1);
     switch(@parse_url($_SERVER['REQUEST_URI'])['path']) {
+        case '/':
+            require 'index.php';
+        case '/index':
+            require 'index.php';
+        case '/index.php':
+            require 'index.php';
         case '/result_contact.php':
             require 'result_contact.php';
         case '/checkout.php':
-            require 'checkout.php';
-        
+            require 'checkout.php';        
     }
 ?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>COS Gym</title>
+<link href="cos-gym.css" rel="stylesheet" type= "text/css" />
+<script src="https://kit.fontawesome.com/42ca52e1de.js" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="src/index.js" type="module"></script>
+</head>
+<body>
+<header >
+<div class="logo">
+  <img src="images/logo_gym.png"></div>
+<div>
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="schedule.html">Schedule</a>
+    <a href="online.html">Online</a>
+    <a href="shop.html">Shop</a>
+    <a href="contact.html">Contact</a>
+ </nav>
+</div> 
+</header>
+<main>
+<div class="imghome">
+  <img src="images/home.jpg">
+  <div class="title-home"><h>COS GYM</h>
+  <a href="login.html"><button class="btn-1">Log in</button></a>
+  <a href="registration.html"><button class="btn-2">Sign up</button></a></div>
+<div class="three-images">
+<figure>
+  <img src="images/main1.jpg">
+<figcaption class="f1">Zumba Dance</figcaption>
+</figure>
+<figure>
+<img src="images/main2.jpg">
+<figcaption class="f2">Power Training</figcaption>
+</figure>
+<figure>
+<img src="images/main3.jpg">
+<figcaption class="f3">Yoga Fitness</figcaption>
+</figure>
+</div>
+</main>
+<footer>
+  <div>
+    <ul>
+     <dt>Email Us</dt>
+     <dt>mail@cosgym.com</dt>
+    </ul>
+  </div>
+   <div>
+     <h3>Welcome to ChrisOlySim Gym</h3>
+     <h4>Contact Us 989-888-9999</h4>
+   </div>
+   <div>
+    <ul>
+     <dt>Follow Us</dt>
+     <dt><i class="fab fa-facebook-f"></i>
+      <i class="fab fa-instagram"></i>
+      <i class="fab fa-twitter"></i>
+    </dt>
+    </ul>
+  </div>
+   </footer>
+</body>
+</html>
+

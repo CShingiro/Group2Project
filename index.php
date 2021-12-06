f79aa2196544c0438e1395a36e94a13927117694
CShingiro <cshingiro7202@conestogac.on.ca>
2021-12-06 15:58:29 -0500

    Bootstrap Menu (with pending fixes)

--- index.php
+++ index.php
@@ -8,22 +8,31 @@
 <script src="https://kit.fontawesome.com/42ca52e1de.js" crossorigin="anonymous"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="src/index.js" type="module"></script>
 </head>
 <body>
 <header >
-<div class="logo">
-  <img src="images/logo_gym.png"></div>
-<div>
-  <nav>
-    <a href="index.html">Home</a>
-    <a href="about.html">About</a>
-    <a href="schedule.html">Schedule</a>
-    <a href="online.html">Online</a>
-    <a href="shop.html">Shop</a>
-    <a href="contact.html">Contact</a>
+<div class="container">
+  <nav font-size="120%" margin-left="30%" margin-top="8%" float="right" class="navbar navbar-expand-xxl navbar-light">
+    <a text-decoration="none" padding="4%" class="navbar-brand">
+      <div class="logo">
+        <img src="images/logo_gym.png">
+      </div>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#toggleMobileMenu" aria-controls="toggleMobileMenu" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="#toggleMobileMenu">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0 text-center">
+        <li><a class="nav-link" href="index.html">Home</a></li>
+        <li><a class="nav-link" href="about.html">About</a></li>
+        <li><a class="nav-link" href="schedule.html">Schedule</a></li>
+        <li><a class="nav-link" href="online.html">Online</a></li>
+        <li><a class="nav-link" href="shop.html">Shop</a></li>
+        <li><a class="nav-link" href="contact.html">Contact</a></li>
+      </ul>
+    </div>
  </nav>
-</div> 
+</div>
 </header>
 <main>
 <div class="imghome">
